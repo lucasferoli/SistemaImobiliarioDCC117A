16b139711cabd422f4872caa8839416c98088fcb
lucasferoli <lucasferoliveira.goval@gmail.com>
2025-05-26 20:40:56 -0300

    Adiciona a coluna faltando, conserta a seeder e factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,14 +20,16 @@ class UserFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
-     */
+'     */
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
+            'role' => fake()->randomElement(['admin', 'user', 'manager']), 
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'created_by' => null,
             'remember_token' => Str::random(10),
         ];
     }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('role');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('password'); // <- adicionado aqui
             $table->foreignId('created_by')->nullable()->constrained('users')->nullOnDelete();
             $table->rememberToken();
             $table->timestamps();
@@ -36,6 +37,7 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+    
     
     }
 
