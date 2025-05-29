733504e5d1d28bb0104b216662ab4dc4260b9e7a
lucasferoli <lucasferoliveira.goval@gmail.com>
2025-05-28 22:02:03 -0300

    Cria factory de propriedades

---
+++ database/factories/PropertyFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Property>
+ */
+class PropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->company(),
+            'description' => fake()->text(),
+            'address' => fake()->unique()->address(),
+            'buyPrice' => fake()->numberBetween(100000, 1000000),
+            'rentPrice' => fake()->numberBetween(1000, 10000),
+            'user_id' => User::inRandomOrder()->first()?->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+    ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Property;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory(10)->create();
+        Property::factory(10)->create();
     }
 }
